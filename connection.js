aee491569264ba14e80acca18ce75c2eb56d4805
root <root@VMONE.ajanmt5y3veehfvhnxsm3f3ftf.bx.internal.cloudapp.net>
2019-10-23 17:01:56 +0000

    El archivo connection tiene el codigo para hacer una consulta, crear un archivo (prueba.json) y leer el mismo

--- connection.js
+++ connection.js
@@ -1,3 +1,5 @@
+const fs=require('fs');
+const parser= require('body-parser');
 const { Pool, Client } = require('pg')
 const pool = new Pool({
   user: 'postgres',
@@ -6,7 +8,22 @@ const pool = new Pool({
   password: 'postgres',
   port: 5432,
 })
-pool.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
+pool.query('SELECT * FROM answers;', (err, res) => {
+  if (err) {
+    console.log(err.stack)
+  } else {
+    const data= JSON.stringify(res.rows);
+    fs.writeFile('prueba.json',data,function(err){
+	if(err){
+	  throw err;
+	}
+	console.log('File was created sucessfully');
+        let lecdata= fs.readFileSync('prueba.json');
+        let json= JSON.parse(lecdata);
+        console.log(json);
+    });
+}
   pool.end()
 })
+
+
