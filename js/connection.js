0925e9980bafbebd02824c6893f3d605a3317fe3
root <root@VMONE.ajanmt5y3veehfvhnxsm3f3ftf.bx.internal.cloudapp.net>
2019-10-25 16:06:02 +0000

    Poniendole orden a todo

--- js/connection.js
+++ js/connection.js
@@ -1,6 +1,10 @@
+/*Programa realizado por Brayan Quirino
+ Archivo que obtiene todos los datos de una tabla y los imprime en un archivo(prueba.js) como en la consola.
+ */
 const fs=require('fs');
 const parser= require('body-parser');
-const { Pool, Client } = require('pg')
+//Pool es la variable que consigue la conexcion con la BD
+const {Pool} = require('pg');
 const pool = new Pool({
   user: 'postgres',
   host: '127.0.0.1',
@@ -8,22 +12,24 @@ const pool = new Pool({
   password: 'postgres',
   port: 5432,
 })
+//Usamos el metodo query
 pool.query('SELECT answer  FROM answers;', (err, res) => {
   if (err) {
     console.log(err.stack)
   } else {
+    //formateamos los datos y escribimos el archivo
     const data= JSON.stringify(res.rows);
-    fs.writeFile('prueba.json',data,function(err){
+    fs.writeFile('../json/prueba.json',data,function(err){
 	if(err){
 	  throw err;
 	}
+	//Abrimos el archivo creado, formateamos los datos e imprimimos.
 	console.log('File was created sucessfully');
-        let lecdata= fs.readFileSync('prueba.json');
+        let lecdata= fs.readFileSync('../json/prueba.json');
         let json= JSON.parse(lecdata);
         console.log(json);
     });
 }
   pool.end()
 })
-
 
--- js/insert.js
+++ js/insert.js
@@ -1,6 +1,9 @@
+/*Programa realizado por Brayan Quirino
+ * Programa que obtine los datos de un JSON y los inserta en la bd examen*/
 const fs=require('fs');
 const parser= require('body-parser');
-const { Pool, Client } = require('pg')
+//La variable Pool es la conexion a la BD
+const { Pool} = require('pg')
 const pool = new Pool({
   user: 'postgres',
   host: '127.0.0.1',
@@ -8,22 +11,20 @@ const pool = new Pool({
   password: 'postgres',
   port: 5432,
 })
-let lecdata= fs.readFileSync('copy.json');
+//Abrimos el archivo
+let lecdata= fs.readFileSync('../json/copy.json');
+//Formateamos los datos
 let trans = JSON.parse(lecdata);
+//Obtenemos el numero de estructuras u objetos JSON dentro del archivo
 let count= Object.keys(trans).length;
 for (i=0; i<count; i++){
+//Obtenemos el dato especifico que nos interesa	
 const json= trans[i].answer;
+//Insertamos en la BD
 pool.query("INSERT INTO answers (answer) VALUES( '" +json+ "' );", (err, res) => {
   if (err) {
     console.log(err.stack)
   } else {
-   /* const data= JSON.stringify(res.rows);
-    fs.writeFile('copy.json',data,function(err){
-	if(err){
-	  throw err;
-	}
-	console.log('JSON created sucessfuly');
-    });*/
     console.log("Insert sucessfuly"+ json);
 }
 })
