b89ff2c67e5dba9167f81e98cfb10f0191ef07a2
Brayan Quirino <dracko.bq@gmail.com>
2019-11-03 04:23:22 +0000

    correccion 16846 lineas

--- js/insert.js
+++ js/insert.js
@@ -13,12 +13,12 @@ let pool = new Pool({
 })
 let string='';
 let anno=['2018','2019','2020'];
-let programa=['E011','E012','E013','E016','E021','E022','E041','E042','E043','P003','R070','S243','S268','S303','U281'];
+let programa=['E010','E011','E012','E013','E016','E021','E022','E041','E042','E043','P003','R070','S243','S268','S303','U281'];
 //Abrimos el archivo
 for(i in anno){
     for (j in programa){
             if(fs.existsSync('../../jsons/salida_'+programa[j]+'_'+anno[i]+'.json')){
-                console.log(anno[i],programa[i]);
+                console.log(anno[i],programa[j]);
                 let lecdata= fs.readFileSync('../../jsons/salida_'+programa[j]+'_'+anno[i]+'.json');
                 //Formateamos los datos
                 let trans = JSON.parse(lecdata);
