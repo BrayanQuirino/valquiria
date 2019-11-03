08fa4af6a52231aa8e7d86ac714a582656267f51
Brayan Quirino <dracko.bq@gmail.com>
2019-11-03 03:34:18 +0000

    ya inserto 16537 lineas

--- js/insert.js
+++ js/insert.js
@@ -1,23 +1,22 @@
 /*Programa realizado por Brayan Quirino
  * Programa que obtine los datos de un JSON y los inserta en la bd examen*/
 const fs=require('fs');
-const path=require('path');
-const parser= require('body-parser');
+//const path=require('path');
 //La variable Pool es la conexion a la BD
-const { Pool} = require('pg')
-const pool = new Pool({
+const {Pool} = require('pg')
+let pool = new Pool({
   user: 'postgres',
   host: '127.0.0.1',
   database: 'valquiria',
   password: 'postgres',
   port: 5432,
 })
+let string='';
 let anno=['2018','2019','2020'];
 let programa=['E011','E012','E013','E016','E021','E022','E041','E042','E043','P003','R070','S243','S268','S303','U281'];
 //Abrimos el archivo
 for(i in anno){
     for (j in programa){
-            
             if(fs.existsSync('../../jsons/salida_'+programa[j]+'_'+anno[i]+'.json')){
                 console.log(anno[i],programa[i]);
                 let lecdata= fs.readFileSync('../../jsons/salida_'+programa[j]+'_'+anno[i]+'.json');
@@ -49,18 +48,18 @@ for(i in anno){
                             array[w]=0;
                         }
                     }
-                    pool.query("INSERT INTO mir (meta,u_admi,nivel,mes,mes_num,programado,acumulado,realizado,diferencia,porcentaje) VALUES( " +array[0]+ ",'"+array[1]+"','"+array[2]+"','"+array[3]+"',"+array[4]+","+array[5]+","+array[6]+","+array[7]+","+array[8]+",'"+array[9]+"%');", (err, res) => {
-                    if (err) {
-                        console.log(err.stack)
-                    }else{
-                        console.log(array);
-                         }
-                    })
+                    string+="INSERT INTO mir (meta,u_admi,nivel,mes,mes_num,programado,acumulado,realizado,diferencia,porcentaje,anno,programa) VALUES( " +array[0]+ ",'"+array[1]+"','"+array[2]+"','"+array[3]+"',"+array[4]+","+array[5]+","+array[6]+","+array[7]+","+array[8]+",'"+array[9]+"%','"+anno[i]+"','"+programa[j]+"');\n"
                 }
         }
     }
 }
+pool.query(string, (err, res) => {
+                    if (err) {
+                        console.log(err.stack)
+                    }
+                    })
 pool.end();
+
 function nombre(mes){
  switch(mes){
   case 1:
